3e9b323df6bd24bf6694ad826a8ce9698a0e5cb4
Yaejun Chung <chunglaos3@gmail.com>
2025-05-29 11:36:42 +0900

    class animal + class lion

--- circle_of_life.py
+++ circle_of_life.py
@@ -1,3 +1,11 @@
+import random
+import os
+import time
+from enum import Enum
+from typing import List, Tuple, Dict, Any
+
+GRID_SIZE=20
+
 class AnimalType(Enum):
     EMPTY:0
     ZEBRA:1
@@ -7,3 +15,23 @@ class Animal:
     def __init__(self, x:int, y:int):
         self.x=x
         self.y=y
+        self.age=0
+        self.alive=True
+
+    def move(self, grid:List[List[AnimalType]], animals:List['Animal'] )->None:
+        return NotImplementedError
+    
+    def breed(self, grid:List[List[AnimalType]], animals:List['Animal'])->None:
+        return NotImplementedError
+    
+    def is_alive(self) -> bool:
+        return self.alive
+    
+    def get_x(self) -> int:
+        return self.x
+    
+    def get_y(self) -> int:
+        return self.y
+    
+    def die(self) -> None:
+        self.alive = False
